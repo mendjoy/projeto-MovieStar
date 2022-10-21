66d4028e713bb5e604093d1ad99b6992448dd566
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-10-21 16:13:27 -0300

    model User

---
+++ models/User.php
@@ -0,0 +1,37 @@
+<?php
+
+    class User{
+
+        public $id;
+        public $lastname;
+        public $email;
+        public $password;
+        public $image;
+        public $bio;
+        public $token;
+    }
+
+    interface UserDAOInterface{
+
+        public function buildUser($data);
+
+        public function create(User $user, $authUser = false);
+
+        public function update(User $user);
+
+        public function verifyToken($protected = false);
+
+        public function setTokenToSession($token, $redirect = true);
+
+        public function authenticateUser($email, $password);
+
+        public function findByEmail($email);
+
+        public function findById($id);
+
+        public function findByToken($token);
+
+        public function changePassoword(User $user);
+    }
+
+?>

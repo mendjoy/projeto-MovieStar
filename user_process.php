22b124bf273cc62120d222b6b85577a37078b2fd
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-10-25 13:53:22 -0300

    ação de editar perfil

--- user_process.php
+++ user_process.php
@@ -1,1 +1,46 @@
 <?php
+
+    require_once("globals.php");
+    require_once("db.php");
+    require_once("models/User.php");
+    require_once("models/Message.php");
+    require_once("dao/UserDAO.php");
+
+    $message = new Message($BASE_URL);
+
+    $userDao = new UserDAO($conn, $BASE_URL);
+
+    // Resgata o tipo do form
+    $type = filter_input(INPUT_POST, "type");
+
+    //atualizar usuario 
+    if($type === "update"){
+
+        //resgata dados do user
+        $userData = $userDao->verifyToken();
+
+        //receber dados do post 
+        $name = filter_input(INPUT_POST, "name");
+        $lastname = filter_input(INPUT_POST, "lastname");
+        $email = filter_input(INPUT_POST, "email");
+        $bio = filter_input(INPUT_POST, "bio");
+
+        //criar um novo objeto de usuario 
+        $user = new User();
+
+        //preencher dados do usuario 
+        $userData->name = $name;
+        $userData->lastname = $lastname;
+        $userData->email = $email;
+        $userData->bio = $bio;
+
+        $userDao->update($userData);
+
+
+
+    //atualizar senha do usuario 
+    } else if ($type === "changepassword"){
+
+    } else{
+        $message->setMessage("Informações inválidas!", "error", "index.php");
+    }

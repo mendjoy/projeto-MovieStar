de5fcaec94a156a84aab18ed1c6a2852876d4cd5
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-10-26 18:48:10 -0300

    dashboard e estilo

---
+++ dashboard.php
@@ -0,0 +1,53 @@
+<?php
+    require_once("templates/header.php");
+    //verifica se o usuario esta logado
+    require_once("dao/UserDAO.php");
+    require_once("models/User.php");
+    
+    $userDao = new UserDAO($conn, $BASE_URL);
+    $user = new User();
+
+    $userData = $userDao->verifyToken(true);
+?>
+
+    <div id="main-container" class="container-fluid">
+        <h2 class="section-title">Dashboard</h2>
+        <p class="section-description">Adicione ou atualize as informações dos filmes que você enviou.</p>
+        <div class="col-md-12" id="add-movie-container">
+            <a href="<?= $BASE_URL?>newmovie.php" class="btn card-btn">
+                <i class="fas fa-plus"></i>Adicionar Filme
+            </a>
+        </div>
+
+        <div class="col-md-12" id="movies-dashboard">
+            <table class="table">
+
+                <thead>
+                    <th scope="col">Id</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Nota</th>
+                    <th scope="col" class="actions-column">Ações</th>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td scope="row">1</td>
+                        <td><a href="#" class="table-movie-title">Título</a></td>
+                        <td><i class="fas fa-star"></i>9</td>
+                        <td class="actions-column">
+                            <a href="" class="edit-btn"><i class="far fa-edit"></i>Editar</a>
+                            <form action="">
+                                <button type="submit" class="delete-btn">
+                                    <i class="fas fa-times"></i>Deletar
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+   
+    </div>
+<?php
+  require_once("templates/footer.php");
+?>

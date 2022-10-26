442d54cb2106dba9455e85eee1f12571cbe7f362
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-10-26 00:12:18 -0300

    home e css da home

--- index.php
+++ index.php
@@ -5,7 +5,42 @@
 
 
     <div id="main-container" class="container-fluid">
+        <h2 class="section-title">Filmes Novos</h2>
+        <p class="section-description">Veja as críticas dos últimos filmes adicionados.</p>
+        <div class="movies-container">
+            <div class="card movie-card">
+                <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/movie_cover.jpg')"></div>
+                <div class="card-body">
+                    <p class="card-rating">
+                        <i class="fas fa-star"></i>
+                        <span class="rating">9</span>
+                    </p>
+                    <h5 class="card-title">
+                        <a href="#">Título do filme</a>
+                    </h5>
+                    <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
+                    <a href="#" class="btn btn-primary card-btn">Conhecer</a>
+                </div>
+            </div>
+        </div>
 
+        <h2 class="section-title">Ação</h2>
+        <p class="section-description">Veja os melhores filmes de ação</p>
+        <div class="movies-container"></div>
+
+        <h2 class="section-title">Comédia</h2>
+        <p class="section-description">Veja os melhores filmes de comédia.</p>
+        <div class="movies-container"></div>
+
+        <h2 class="section-title">Documentário</h2>
+        <p class="section-description">Veja os melhores documentários.</p>
+        <div class="movies-container"></div>
+
+        <h2 class="section-title">Drama</h2>
+        <p class="section-description">Veja os melhores filmes de drama.</p>
+        <div class="movies-container"></div>
+
+    
     </div>
 
 <?php

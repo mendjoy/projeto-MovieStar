98d1071d7da2ba1866c72d656ba2cf57bc829747
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-10-28 15:55:13 -0300

    funcionalidade de adicionar comentário

--- review_process.php
+++ review_process.php
@@ -3,9 +3,11 @@
     require_once("db.php");
     require_once("models/User.php");
     require_once("models/Message.php");
+    require_once("models/Review.php");
     require_once("models/Movie.php");
     require_once("dao/UserDAO.php");
     require_once("dao/MovieDAO.php");
+    require_once("dao/ReviewDAO.php");
 
     //recebe o tipo do form
     $type = filter_input(INPUT_POST, "type");
@@ -13,9 +15,49 @@
     $message = new Message($BASE_URL);
     $userDao = new UserDAO($conn, $BASE_URL);
     $movieDao = new MovieDAO($conn, $BASE_URL);
+    $reviewDao = new ReviewDAO($conn, $BASE_URL);
+
 
     $userData = $userDao->verifyToken();
 
     if($type === "create"){ 
+        //receber dados do post
 
+        $users_id= filter_input(INPUT_POST, "rating");
+        $review = filter_input(INPUT_POST, "review");
+        $movies_id = filter_input(INPUT_POST, "movies_id");
+        $users_id = $userData->id;
+
+        $reviewObject = new  Review();
+
+        $movieData = $movieDao->findById($movies_id);
+
+        //validando se o filme existe
+        if($movieData){
+
+            //verificar dados minimos 
+            if(!empty($rating) &&  !empty($review) && !empty($movies_id)){
+
+                $reviewObject->rating = $rating;
+                $reviewObject->review = $review;
+                $reviewObject->movies_id = $movies_id;
+                $reviewObject->users_id= $users_id;
+
+                $reviewDao->create($reviewObject);
+
+
+            } else{
+                $message->setMessage("Você precisa inserir a nota e o comentário!", "error", "back");
+            }
+
+        } else{
+
+            $message->setMessage("Informações inválidas!", "error", "index.php");
+
+        }
+
+
+    } else {
+
+        $message->setMessage("Informações inválidas!", "error", "index.php");
     }
